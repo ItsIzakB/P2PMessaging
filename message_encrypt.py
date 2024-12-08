8c5ff0a89b49600784f45c4216771b8d2ea8ee0e
izak <kazipriom@gmail.com>
2024-12-07 20:16:28 -0600

    DES

--- message_encrypt.py
+++ message_encrypt.py
@@ -1,8 +1,17 @@
 import key_encrypt as kdf
+import secrets
+from Crypto.Cipher import DES
 
 password = b'password'
 
 
 key = kdf.derive_key(password)
 
-kdf.verify_pass('password', key)
+block_size = 7 # 56-bits for DES
+
+iv = secrets.token_bytes(block_size)
+
+print(f"Generated IV : {iv}")
+
+
+
