23370e11facfee3b18637ca3c01c702142d71f60
izak <kazipriom@gmail.com>
2024-12-07 18:43:07 -0600

    cryptography

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
+import os
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+salt = os.urandom(16)
 
 #derive
 
 kdf = PBKDF2HMAC (
     algorithm = hashes.SHA256(),
-    length =
+    length = 32,
+    salt = salt,
+    iterations=10000,
+)
+
+
+key =  kdf.derive(b"password")
+
+kdf = PBKDF2HMAC(
+    algorithm = hashes.SHA256(),
+    length = 32,
+    salt = salt,
+    iterations = 10000
 )
