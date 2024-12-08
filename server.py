6a18f8d25ff233b0cc5264842d3e998948265eac
izak <kazipriom@gmail.com>
2024-12-08 01:17:51 -0600

    encryption

--- server.py
+++ server.py
@@ -1,13 +1,40 @@
 import socket
 
+def handle_clients(client1, client2):
+    while True:
+        msg = client1.recv(1024)
+
+        if not msg:
+            print("Client 1 disconnected")
+            break
+
+        client2.send(msg)
+
+        msg = client2.recv(1024)
+
+        if not msg:
+            print("Client 2 disconnected")
+            break
+        client1.send(msg)
+
+
+    client1.close()
+    client2.close()
+
+
+
+
+
+
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 server_socket.bind(('127.0.0.1', 12345))
 
 server_socket.listen(2)
 
-print("Server: Waiting for chatters to join")
+print("Server: Waiting for 2 people to join")
 
+cli
 conn, addr = server_socket.accept()
 
 print('Successful connection')
