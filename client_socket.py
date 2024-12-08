51879db50bdf01e958b85ed03300d8f24b1d7a49
izak <kazipriom@gmail.com>
2024-12-07 23:26:49 -0600

    message_decrypt

--- client_socket.py
+++ client_socket.py
@@ -1,8 +1,13 @@
 import socket
+import message_encrypt_and_decrypt as med
+import key_encrypt as kdf
 
 
 print("Enter your Name: ")
 name = input()
+print("Enter Password: ")
+password = input()
+
 c_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 c_socket.connect(('127.0.0.1', 12345))
 
@@ -10,8 +15,15 @@ print("Start chatting!")
 
 while True:
 
-    message = input(f"{name}: ")
-    c_socket.send(message)
+    message = input()
+    encoded_message = message.encode()
+
+    key = kdf.derive_key(password)
+
+    encrypted_message = med.message_encrypt(key, encoded_message)
+
+
+    c_socket.send(encrypted_message)
 
     if message.lower() == 'exit':
         print("Exiting the application")
---
+++ message_encrypt_and_decrypt.py
@@ -0,0 +1,48 @@
+import key_encrypt as kdf
+import secrets
+from Crypto.Cipher import DES
+from Crypto.Util.Padding import pad,unpad
+import base64
+
+
+
+
+def message_encrypt(key, plaintext):
+
+    block_size = 8 # 56-bits for DES
+
+    iv = secrets.token_bytes(block_size)
+
+    print(f"Generated IV : {iv}")
+
+    cipher = DES.new(key, DES.MODE_CBC, iv=iv)
+
+
+    #using a pkcs7 padding style
+    padded_plaintext = pad(plaintext, DES.block_size)
+
+    ciphertext = cipher.encrypt(padded_plaintext)
+    print(f"Raw CipherText:  {ciphertext}")
+    print(f"Hex CipherText: {ciphertext.hex()}")
+
+    return ciphertext
+
+
+def message_decrypt(key, encrypted_message):
+    iv_and_ciphertext = base64.b64decode(encrypted_message)
+    iv = iv_and_ciphertext[:8]
+    ciphertext = iv_and_ciphertext[8:]
+
+    cipher = DES.new(key, DES.MODE_CBC, iv)
+
+    padded_plaintext = cipher.decrypt(ciphertext)
+
+    plaintext = unpad(padded_plaintext, DES.block_size)
+
+    return plaintext.decode()
+
+
+
+
+
+
