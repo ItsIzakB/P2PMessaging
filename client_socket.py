d3616a9f4c00416208a20907b89e9a99d33ce756
izak <kazipriom@gmail.com>
2024-12-08 01:49:07 -0600

    encryption

--- client_socket.py
+++ client_socket.py
@@ -3,41 +3,37 @@ import message_encrypt_and_decrypt as med
 import key_encrypt as kdf
 
 
-print("Enter your Name: ")
-name = input()
-print("Enter Password: ")
-password = input()
-password = b'password'
-print(password)
+class client_socket:
+    def __init__(self, name, password):
+        self.name = name
+        self.password = password
+        password = b'password'
+        print(password)
 
-c_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-c_socket.connect(('127.0.0.1', 12345))
+        c_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        c_socket.connect(('127.0.0.1', 12345))
 
-print("Start chatting!")
+        print("Start chatting!")
 
-while True:
+        while True:
 
-    message = input()
-    encoded_message = message.encode()
+            message = input()
+            encoded_message = message.encode()
 
-    key = kdf.derive_key(password)
+            key = kdf.derive_key(password)
 
-    encrypted_message = med.message_encrypt(key, encoded_message)
+            encrypted_message = med.message_encrypt(key, encoded_message)
 
+            c_socket.send(encrypted_message)
 
-    c_socket.send(encrypted_message)
+            if message.lower() == 'exit':
+                print("Exiting the application")
+                break
 
-    if message.lower() == 'exit':
-        print("Exiting the application")
-        break
+            response = c_socket.recv(1024)
 
-    response = c_socket.recv(1024)
+            message = med.message_decrypt(response)
 
+            print(f'Server: {message.decode()}')
 
-
-    print(f'Server: {response.decode()}')
-
-c_socket.close()
-
-
-
+        c_socket.close()
---
+++ main.py
@@ -0,0 +1,17 @@
+from client_socket import client_socket
+import key_encrypt
+import message_encrypt_and_decrypt
+
+
+def alice_client():
+    alice = client.sock
+
+def main():
+
+    alice = client_socket('alice', b'12345')
+    bob = client_socket('bob', b'12345')
+
+
+
+if __name__ == '__main__':
+    main()
