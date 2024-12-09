9f96d78af16b43b7c40a2d77da7a56642900b60d
izak <kazipriom@gmail.com>
2024-12-09 17:16:10 -0600

    react app creation

--- p2pmessage-app/src/App.js
+++ p2pmessage-app/src/App.js
@@ -5,12 +5,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>header</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        Hi this is my app now
-        </p>
+        <h1> P2P Messaging App </h1>
       </header>
+      <main>
+
+      <div id = "chat-window">
+
+
+
+      </div>
+
+
+      </main>
     </div>
   );
 }
