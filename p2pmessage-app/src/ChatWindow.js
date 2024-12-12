e110f57a5830785b59ebd759dcb5ffd40248faed
izak <kazipriom@gmail.com>
2024-12-11 21:36:05 -0600

    chatwindow

--- p2pmessage-app/src/ChatWindow.js
+++ p2pmessage-app/src/ChatWindow.js
@@ -2,8 +2,12 @@ import React from "react"
 
 function ChatWindow({messages}){
     return(
-        <div id="chat-window"></div>
-    )
+        <div id="chat-window">
+           {messages.map((msg, index) => (
+           <p key = {index}>{msg}</p>
+           ))} 
+        </div>
+    );
 
 
 }
