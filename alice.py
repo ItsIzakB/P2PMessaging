ae1aebac1d8b23b4e9753c7c5dbc4752bb8eb9d0
Kazi Priom <109478175+ItsIzakB@users.noreply.github.com>
2024-12-31 23:44:02 -0600

    Update alice.py

--- alice.py
+++ alice.py
@@ -1,5 +1,5 @@
 from client import Client
-
+#wrong password
 try:
     alice = Client('Alice', b'wrong_pass', b'\x12\x34\x56\x78\x9a\xbc\xde\xf0\x11\x22\x33\x44\x55\x66\x77\x88')
     alice.start()
